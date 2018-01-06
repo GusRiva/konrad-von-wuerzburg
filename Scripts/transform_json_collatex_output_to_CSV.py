945ee9636fb68b71200af942408bd2ad770a1a17
GusRiva <gusfer@gmail.com>
2018-01-06 11:57:49 -0300

    CSV for Stemma in column

--- Scripts/transform_json_collatex_output_to_CSV.py
+++ Scripts/transform_json_collatex_output_to_CSV.py
@@ -6,20 +6,41 @@ import json
 #complete with the source path
 source = "welt_lohn_collation1.json"
 #complete with the name of the output file
-output_file = "welt_lohn_collation1.csv"
+output_file = "welt_lohn_full.csv"
 
-data = json.load(codecs.open("welt_lohn_collation1.json", 'r', 'utf-8'))
+data = codecs.open(source, 'r','utf-8-sig')
+data = json.load(data)
+
 outfile = codecs.open(output_file, 'w', 'utf-8')
 index = 0
 for wit in data['witnesses']:
 	outfile.write(wit)
-	outfile.write(",")
-	for elem in data['table'][index]:
-		if elem != None:
-			outfile.write(elem[0]['t'])
-			outfile.write(",")
+	outfile.write(',')
+outfile.write('\n')
+
+number_wits = len(data['witnesses'])
+number_words = len(data['table'][0])
+index = 1
+for word in range(number_words):
+	for wit in range(number_wits):
+		token = data['table'][wit][word]
+		if token != None:
+			outfile.write(token[0]['t'])
+			outfile.write(',')
 		else:
-			outfile.write(",")
+			outfile.write(',')
+	outfile.write('\n')
 	index = index + 1
-	outfile.write("\n")
+	
+
+
+
+# 	for elem in data['table'][index]:
+# 		if elem != None:
+# 			outfile.write(elem[0]['t'])
+# 			outfile.write(",")
+# 		else:
+# 			outfile.write(",")
+# 	index = index + 1
+# 	outfile.write("\n")
 outfile.close()
