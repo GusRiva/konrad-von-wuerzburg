87975b187c91d627e2bfcb71e1a8a2279a8450e8
GusRiva <gusfer@gmail.com>
2018-04-26 23:14:18 -0300

    Website ready

--- Website/database.php
+++ Website/database.php
@@ -2,15 +2,15 @@
 
 $textDict = array('herz' => array(
 							'title' => 'Herzmaere',
-							'file' => 'TEI/Herz_Synoptische_Transkription.xml',
+							'file' => 'TEI/Herz_syn.xml',
 							'crit_text' => ['P(A)','V']),
 				'dwl'=> array(
 							'title'=>'Der Welt Lohn',
-							'file'=>'TEI/DWL_Synoptische_Transkription.xml',
+							'file'=>'TEI/DWL_syn.xml',
 							'crit_text'=> ['M']),
 				'hvk'=> array(
 							'title'=>'Heinrich von Kempten',
-							'file'=>'TEI/HvK_Synoptische_Transkription.xml',
+							'file'=>'TEI/HvK_syn.xml',
 							'crit_text'=>['P']));
 
 return $textDict;
